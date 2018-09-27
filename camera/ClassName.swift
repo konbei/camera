1cca2bd5696f0d95f8b3e2ee43fb8aa785c27059
中西航平 <kouheinakanisi@gmail.com>
2018-09-27 17:34:53 +0900

    名前設定画面の追加（設定した名前、時間はシーン移動すると初期化される状態)

--- camera/ClassName.swift
+++ camera/ClassName.swift
@@ -8,83 +8,87 @@
 
 import UIKit
 
-class ClassName: UITableViewController {
+class ClassName: UITableViewController,TextEditedDelegate{
+    
+    
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    
+    //TableViewで扱う空の配列(ここで初期化したらコンパイル時にエラーはいたのでViewDidLoadで初期化
+    var sectionTitles: [String] = []
+    public var rowTitles:[String] = []
+    public var rowData:[String] = []
+    public var cellData:[[String]] = []
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+       //行数を返す
+        return rowTitles.count
+        
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
-    }
-
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
-    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+    
+    override   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        //各行に表示するセルを返す
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)  as! ClassNameTableCell //自作セル登録
+        let rowTitle = rowTitles[indexPath.row]
+        cell.ClassNumber!.text = rowTitle  //時限セット
+        
+        let name = cellData[indexPath.section][indexPath.row]
+        cell.ClassNameText!.text = name      //名前セット
+        
+        cell.delegate = self
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func numberOfSections(in tableView: UITableView) -> Int // Default is 1 if not implemented
+    {
+        //セクション数を返す
+        return sectionTitles.count
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String?
+    {
+        //セクションタイトルを返す
+        return sectionTitles[section]
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        //テーブルデータ初期化
+        sectionTitles = ["月曜","火曜","水曜","木曜","金曜"]
+        rowTitles = ["1限","2限","3限","4限","5限","6限"]
+        rowData = ["1限の名前を入力して下さい","2限の名前を入力して下さい","3限の名前を入力して下さい","4限の名前を入力して下さい","5限の名前を入力して下さい","6限の名前を入力して下さい"]
+        cellData = [rowData,rowData,rowData,rowData,rowData]
+        
+        //自作セル追加
+        let classXib = UINib(nibName:"ClassNameTableCell", bundle:nil)
+        tableView.register(classXib, forCellReuseIdentifier:"Cell")
+        
     }
-    */
+    
 
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+    
+    //returnキー押した後の動作(ClassNameTableCellのtextfieldデリゲートメソッドtextFieldDidEndEditing(return内のキーを押した後の動作)からデリゲート)
+    func textFieldDidEndEditing(cell: ClassNameTableCell, value: String) {
+        let index = tableView.indexPathForRow(at:cell.convert(cell.bounds.origin, to:tableView))//タップしたらタップしたセルのインデックスを得る(動作確認済み)
+        
+        cellData[index!.section][index!.row] = value
+        tableView.reloadData()
     }
-    */
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     
+     */
+    
 }
+
---
+++ camera/ClassNameTableCell.swift
@@ -0,0 +1,56 @@
+//
+//  ClassNameTableCell.swift
+//  camera
+//
+//  Created by 中西航平 on 2018/09/27.
+//  Copyright © 2018 kohei nakanishi. All rights reserved.
+//
+
+import UIKit
+
+protocol TextEditedDelegate {
+    func textFieldDidEndEditing(cell:ClassNameTableCell, value:String)
+}
+class ClassNameTableCell: UITableViewCell,UITextFieldDelegate
+{
+    
+    var delegate:TextEditedDelegate! = nil
+    
+    
+    //自作セル(.xib)のitem
+    @IBOutlet weak var ClassNumber: UILabel!
+    @IBOutlet weak var ClassNameText: UITextField!
+    
+    //nibファイルがアプリケーションに読み込まれ、nibファイルに登録されたのオブジェクト間のインスタンス変数(IBOutlet)の自動接続が終了すると送信されるメッセージ
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+        
+    }
+    
+    //主にビューのフレームが変更された時に呼ばれる
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        ClassNameText.delegate = self   //textfieldからデリゲート
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+    
+    
+    //returnキーを押した後の動作
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool
+    {
+        //キーボードを閉じる。
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    //returnキー押した後の動作(shouldreturnの後に呼ばれる)
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        //ClassNameCell.deregate = selfされたクラスにtextFieldDidEnding実行されたとを通知
+                                            //   のtextFieldDidEnding実行
+        self.delegate.textFieldDidEndEditing(cell: self, value:textField.text!)
+    }
+}
