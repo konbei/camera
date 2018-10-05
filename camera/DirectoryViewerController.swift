ced4c518f09c7a47cf2a6873d41868b62cd76d91
中西航平 <kouheinakanisi@gmail.com>
2018-10-05 20:36:08 +0900

    -写真読み込みを非同期で行うよう変更

--- camera/DirectoryViewerController.swift
+++ camera/DirectoryViewerController.swift
@@ -8,6 +8,25 @@
 
 import UIKit
 
+//リサイズ方法拡張
+extension UIImage {
+    func resize(size _size: CGSize) -> UIImage? {
+        let widthRatio = _size.width / size.width
+        let heightRatio = _size.height / size.height
+        let ratio = widthRatio < heightRatio ? widthRatio : heightRatio
+        
+        let resizedSize = CGSize(width: size.width * ratio, height: size.height * ratio)
+        
+        UIGraphicsBeginImageContextWithOptions(resizedSize, true, 0.0)
+        draw(in: CGRect(origin: .zero, size: resizedSize))
+        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return resizedImage
+    }
+}
+
+
 class DirectoryViewerController: UIViewController,UICollectionViewDataSource,
 UICollectionViewDelegate {
     
@@ -34,46 +53,53 @@ UICollectionViewDelegate {
         if fileNames.count != 0{
             for i in 0..<fileNames.count{
                 let fileDirectory = holderDirectory + "/" + fileNames[i]
-            fileImages.append(UIImage(contentsOfFile:fileDirectory)!)
+            fileImages.append( UIImage(contentsOfFile:fileDirectory)!)
             }
         }
         
     }
     
+
+
+    
     func collectionView(_ collectionView: UICollectionView,
                         cellForItemAt indexPath: IndexPath) -> UICollectionViewCell{
         
-        // "Cell" はストーリーボードで設定したセルのID
-        let cell:UICollectionViewCell =
-            collectionView.dequeueReusableCell(withReuseIdentifier: "Cell",
-                                               for: indexPath)
+        //セルのクラス作ってそこでスクロールした時の初期化実装してます
+        let cell:DirectoryViewrCell =
+            collectionView.dequeueReusableCell(withReuseIdentifier: "Cell",for: indexPath) as! DirectoryViewrCell
+
+       
+        let cellImage = self.fileImages[indexPath.row]
+        var thumbnail:UIImage? = nil
+        let group = DispatchGroup()
+        let queue = DispatchQueue(label: "imageSetting",attributes: .concurrent)
+        group.enter()
+        queue.async(group: group) {
+            //サムネイル作成方法変更
+            thumbnail = cellImage.resize(size: CGSize(width: 81, height: 81))
+                group.leave()
+            }
+            group.notify(queue: .main){
+                cell.thumnailImagre.image = thumbnail
+            }
         
-        // Tag番号を使ってImageViewのインスタンス生成
-        let imageView = cell.contentView.viewWithTag(1) as! UIImageView
-        if (2*indexPath.section + indexPath.row) < fileImages.count{
-            let cellImage = fileImages[2*indexPath.section + indexPath.row]
-        
-            imageView.image = cellImage
-        }
         return cell
     }
+
+    
+
+    
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        
-        
-        // section数は１つ
-        if Int(ceil(Double(fileImages.count / 2))) != 0{
-            return Int(ceil(Double(fileImages.count) / 2.0))
-        }
-        
-        return 0
+        //よくよく考えたら1セクションあたりのあセル表示数を固定する必要ないと思ったので変更
+        return 1
     }
     
     func collectionView(_ collectionView: UICollectionView,
                         numberOfItemsInSection section: Int) -> Int {
         // 要素数を入れる、要素以上の数字を入れると表示でエラーとなる
-        
-        return 2
+        return fileNames.count
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -101,3 +127,4 @@ UICollectionViewDelegate {
     */
 
 }
+
---
+++ camera/DirectoryViewrCell.swift
@@ -0,0 +1,20 @@
+//
+//  DirectoryViewrCellCollectionViewCell.swift
+//  camera
+//
+//  Created by 中西航平 on 2018/10/04.
+//  Copyright © 2018 kohei nakanishi. All rights reserved.
+//
+
+import UIKit
+
+class DirectoryViewrCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumnailImagre: UIImageView!
+    
+    //セルが再利用される時呼ばれる、このタイミングで初期化する
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        thumnailImagre.image = nil
+    }
+}
